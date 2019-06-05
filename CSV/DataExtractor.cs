572f8f8fff6f86d170afd29efbcc9b4f20981c86
Benoît Geslain <benoitgeslain@gmail.com>
2019-06-05 17:37:31 +0300

    CSV parser added

---
+++ CSV/DataExtractor.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DataExtractor {
+
+	ExperimentManager manager;
+
+	public DataExtractor() {
+		GameObject exp = GameObject.Find("Experiment");
+		if (exp == null) {
+			Debug.Log("/!\\ Error:: can't find experiment gameobject");
+		}
+		manager = exp.GetComponent<ExperimentManager>();
+		if (manager == null) {
+			Debug.Log("/!\\ Error:: can't find ExperimentManager gameobject");
+		}
+	}
+
+	public string[] GetTracebackData() {
+		var tmp = manager.GetCurrentTrial();
+		return new string[] {tmp[0], tmp[1], tmp[2], tmp[3], Time.time.ToString("N3")};
+	}
+
+	public string[] GetExperimentData() {
+		var tmp = manager.GetCurrentTrial();
+		return new string[] {tmp[0], tmp[1], tmp[2], tmp[3], Time.time.ToString("N3")};
+	}
+}
